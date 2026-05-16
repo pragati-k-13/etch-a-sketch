9927661fa31d8b1b51e698588e217163dd859b4f
Pragati <214149956+pragati-k-13@users.noreply.github.com>
2026-05-16 12:44:51 +0530

    increase cell transparency by 0.1 on hover

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function createGrid(numberOfDivs) {
     // Create divs per row
     let htmlContent = "";
     for (let i = 0; i < numberOfDivs; i++) {
-      htmlContent += `<div class="grid-div" data-hover-color="hsl(${Math.random() * 360},60%,40%)"></div>`;
+      htmlContent += `<div class="grid-div" data-bg-color="hsl(${Math.random() * 360},60%,40%)"></div>`;
     }
     gridRow.innerHTML = htmlContent;
     grid.appendChild(gridRow);
@@ -19,42 +19,41 @@ function createGrid(numberOfDivs) {
 
 createGrid(4);
 
-// Random colours on hover
 const eachDiv = document.querySelectorAll(".grid-div");
 for (let i = 0; i < eachDiv.length; i++) {
+  eachDiv[i].style.backgroundColor = eachDiv[i].dataset.bgColor;
   eachDiv[i].addEventListener("mouseenter", () => {
-    eachDiv[i].style.backgroundColor = eachDiv[i].dataset.hoverColor;
-  });
-
-  eachDiv[i].addEventListener("mouseleave", () => {
-    eachDiv[i].style.backgroundColor = "#ffffff";
+    let currentOpacity = parseFloat(eachDiv[i].style.opacity) || 0.1;
+    if (currentOpacity < 1) {
+      eachDiv[i].style.opacity = currentOpacity + 0.1;
+      console.log(currentOpacity);
+    }
   });
 }
-
-// Random colours on hover
-// const eachDiv = document.querySelectorAll(".grid-div");
-// for (let i = 0; i <= eachDiv.length; i++) {
-//   eachDiv[i].addEventListener("mouseenter", () => {
-//     const randomColor = `hsl(${Math.floor(Math.random() * 255).toString()}, 50%, 90%)`;
-//     eachDiv[i].style.backgroundColor = randomColor;
-//     let currentOpacity = parseFloat(eachDiv[i].style.opacity);
-//     if (currentOpacity < 100) {
-//       eachDiv[i].style.opacity = currentOpacity + 10;
-//     }
-//   });
-
-//   eachDiv[i].addEventListener("mouseleave", () => {
-//     eachDiv[i].style.backgroundColor = "#ffffff";
-//   });
-// }
 
 button.addEventListener("click", () => {
   let size = prompt("How many boxes per side?");
 
-  if (size > 100) {
+  if (size === null) {
+    return;
+  } else if (size == 0) {
+    alert("Number cannot be 0! Try again with a number between 1 and 100.");
+  } else if (size > 100) {
     alert("Number cannot be more than 100!");
   } else {
     grid.replaceChildren();
     createGrid(size);
+
+    const eachDiv = document.querySelectorAll(".grid-div");
+    for (let i = 0; i < eachDiv.length; i++) {
+      eachDiv[i].style.backgroundColor = eachDiv[i].dataset.bgColor;
+      eachDiv[i].addEventListener("mouseenter", () => {
+        let currentOpacity = parseFloat(eachDiv[i].style.opacity) || 0.1;
+        if (currentOpacity < 1) {
+          eachDiv[i].style.opacity = currentOpacity + 0.1;
+          console.log(currentOpacity);
+        }
+      });
+    }
   }
 });
